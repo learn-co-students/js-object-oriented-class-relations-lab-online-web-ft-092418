dfc37a207a02fc128a21465b867b14c9f6975c0c
larryvsdata <larryvsdata@gmail.com>
2019-01-15 18:23:43 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,105 @@
+let store = { drivers: [], passengers: [], trips: [] };
+
+
+let driverId = 0;
+
+class Driver {
+    constructor(name) {
+        this.id = ++driverId;
+        this.name = name;
+
+        // insert in the user to the store
+        store.drivers.push(this);
+    }
+
+    trips() {
+    return store.trips.filter(
+        function(trip) {
+            return trip.driverId === this.id;
+        }.bind(this)
+    );
+  }
+
+
+  passengers(){
+
+    return this.trips().map( function(trip) {
+        return trip.passenger() ;
+    }.bind(this));
+  }
+
+
+
+}
+
+
+let passengerId = 0;
+
+class Passenger {
+    constructor(name) {
+        this.id = ++passengerId;
+        this.name = name;
+
+        // insert in the user to the store
+        store.passengers.push(this);
+    }
+
+
+    trips() {
+    return store.trips.filter(
+        function(trip) {
+            return trip.passengerId === this.id;
+        }.bind(this)
+    );
+  }
+
+  drivers(){
+
+    return this.trips().map( function(trip) {
+        return trip.driver() ;
+    }.bind(this));
+  }
+
+
+
+}
+
+
+
+
+
+let tripId = 0;
+
+class Trip {
+    constructor( driver , passenger ) {
+        this.id = ++tripId;
+        this.name = name;
+        this.setDriver(driver);
+        this.setPassenger(passenger);
+        store.trips.push(this);
+    }
+    setDriver(driver) {
+        this.driverId = driver.id;
+    }
+
+    setPassenger(passenger) {
+        this.passengerId = passenger.id;
+    }
+
+    driver() {
+    return store.drivers.find(
+        function(driver) {
+            return driver.id === this.driverId;
+        }.bind(this)
+    );
+  }
+
+  passenger() {
+  return store.passengers.find(
+      function(passenger) {
+          return passenger.id === this.passengerId;
+      }.bind(this)
+  );
+}
+
+}
